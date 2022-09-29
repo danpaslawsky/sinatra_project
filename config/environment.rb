3503e98b5bd50b532d66e043773db2fb60465900
Dan Paslawsky <danpaslawsky@gmail.com>
2022-09-29 13:57:41 -0700

    file structure and initial setup

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require 'bundler'
+require 'rubygems'
+require 'sinatra'
+
+Bundler.require
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/development.sqlite.3"
+)
+
+require_all 'app'
