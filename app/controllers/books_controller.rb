fa3ec41d5bb0103d6a89997580f3532c2153efb2
Dan Paslawsky <danpaslawsky@gmail.com>
2022-10-07 20:36:00 -0700

    in BooksController get /route/new to create new form to add a new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,22 +3,22 @@ class BooksController < ApplicationController
 
     #our user just requested all the books
     # index route
-    get '/posts' do
+    get '/books' do
       #params = {"id"=>"1"}
-      @books = Book.all
-      erb :'books/index' #render the index view/file
+        @books = Book.all
+        erb :'books/index' #render the index view/file
     end 
 
+    # my user just made a request to view form to add a new book
+    get '/books/new' do
+        erb :'books/new'
+    end    
+    
     # our user wants to see details of One book
     # show route
-    get '/posts/:id' do
-      #binding.pry
-      @book = Book.find(params[:id])
-
-      erb :'books/show'
+    get '/books/:id' do
+        @book = Book.find(params[:id])
+        erb :'books/show'
     end
-
-    
-
 
 end
