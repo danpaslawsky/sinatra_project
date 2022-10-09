bc67c88a2398d8fbfb955577e61a7f0c30655fa5
Dan Paslawsky <danpaslawsky@gmail.com>
2022-10-08 19:39:04 -0700

    creation of all the Book Views and layout file, completion of Book controller routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < Sinatra::Base
-    #all other controllers will inherit from this parent controller
+    # all other controllers will inherit from this parent controller
+    # the way that our controllers communicate with views is through instance vaiables
+    # order of routes is important
 
-    #configure block to set path to views 
+    # configure block to set path to views 
     configure do 
         #sessions will be set and security
         #set :public_folder, 'public'
         set :views, 'app/views'
     end
 
-# order of routes is important
-    #define general routes that dont involve a model
-    #route route '/'
-    get '/' do
-        "welcome"
-    end 
 
+    # define general routes that dont involve a model
+    # route route '/'
+    get '/' do
+        erb :welcome
+    end 
 
 end    
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,57 @@
 class BooksController < ApplicationController
-    #all routes containing to books model
+    # all routes containing to books model
 
-    #our user just requested all the books
+    # user just requested all the books
     # index route
     get '/books' do
-      #params = {"id"=>"1"}
         @books = Book.all
         erb :'books/index' #render the index view/file
     end 
 
-    # my user just made a request to view form to add a new book
+    # user just made a request to view form to add a new book
     get '/books/new' do
+
         erb :'books/new'
     end    
     
-    # our user wants to see details of One book
+    # order matters when building routes. Get dynamic routes need to be after all other get routes
+    # user wants to see details of One book
     # show route
     get '/books/:id' do
-        @book = Book.find(params[:id])
+        @book = Book.find_by(id:params[:id])
         erb :'books/show'
+    end
+
+    post '/books' do
+        #create new post
+        #redirect user somewhere 
+        @book = Book.create(params)
+        redirect "/books/#{@book.id}"
+    end
+
+    # user just requested to see an edit form for a post
+    get '/books/:id/edit' do
+        @book = Book.find_by(id:params[:id])
+        erb :"/books/edit"
+
+    end
+
+    # user just submitted the edit form
+    patch '/books/:id' do
+        @book = Book.find_by(id:params[:id])
+
+        @book.update(title: params[:title], author: params[:author], book_genre: params[:book_genre], number_of_pages: params[:number_of_pages], book_review: params[:book_review])
+        redirect "/books/#{@book.id}"
+        #no view
+        # update the particular obj w/ new attribute
+    end 
+
+    # user wants to delete an existing post
+    delete '/books/:id' do
+        @book = Book.find_by(id:params[:id])
+        @book.destroy
+        redirect '/books'
+      #no view
     end
 
 end
